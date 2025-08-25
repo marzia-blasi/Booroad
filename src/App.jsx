2c337624809b3187c0a79c873d0112544899d7ee
Your Name <you@example.com>
2025-08-25 18:10:12 +0200

    add Footer component and integrate it into the default layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import DefaultLayout from './Layouts/DefaultLayout';
 import HomePage from "./Pages/HomePage";
 
+
 export default function Main() {
 
   return (
--- src/Layouts/DefaultLayout.jsx
+++ src/Layouts/DefaultLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
+import Footer from "../Components/Footer";
 
 export default function DefaultLayout() {
 
     return (
-       
-        <Outlet />
-
+        <>
+            <Outlet />
+            <Footer />
+        </>
     )
 
 }
