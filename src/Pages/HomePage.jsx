db38c43f6b1ec0859c1e70056d48af24ee1865cb
tommaso-mandorino <tommasomandorino@outlook.it>
2025-08-25 17:52:35 +0200

    add and use default layout

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import Jumbotron from "../Components/Jumbotron";
+
+export default function HomePage() {
+
+    return (
+
+        <>
+        
+            <Jumbotron />
+
+            <h1>Booroad</h1>
+
+        </>
+
+    );
+
+}
