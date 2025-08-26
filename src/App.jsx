44721cb268f84490c918c9587a8d23021d727b95
simone-sampino <s.samp98@gmail.com>
2025-08-26 12:24:22 +0200

    add tripslist page

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 
-import DefaultLayout from './Layouts/DefaultLayout';
+import DefaultLayout from "./Layouts/DefaultLayout";
 
 import HomePage from "./Pages/HomePage";
 import Error404 from "./Pages/Error404";
+import TripsList from "./Pages/TripsList";
 
 export default function Main() {
-
   return (
-
     <BrowserRouter>
-
       <Routes>
-
         <Route element={<DefaultLayout />}>
-
           <Route index element={<HomePage />} />
 
+          <Route path="/tripslist" element={<TripsList />} />
+
           <Route path="*" element={<Error404 />} />
-
         </Route>
-
       </Routes>
-
     </BrowserRouter>
-
   );
-
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,13 @@
+import { NavLink } from "react-router";
+
 export default function Header() {
   return (
     <>
-      <nav class="navbar navbar-expand-sm navbar-light bg-light">
-        <div class="container">
-          <img src="../public/logo.png" alt="" />
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <div className="container">
+          <img src="../logo.png" alt="" />
           <button
-            class="navbar-toggler d-lg-none"
+            className="navbar-toggler d-lg-none"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#collapsibleNavId"
@@ -13,30 +15,34 @@ export default function Header() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="collapsibleNavId">
-            <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" href="#" aria-current="page">
+          <div className="collapse navbar-collapse" id="collapsibleNavId">
+            <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" href="#" aria-current="page">
                   Home
-                  <span class="visually-hidden">(current)</span>
+                  <span className="visually-hidden">(current)</span>
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item">
+                <NavLink
+                  className="nav-link"
+                  to="/tripslist"
+                  aria-current="page"
+                >
                   Lista Viaggi
-                </a>
+                </NavLink>
               </li>
             </ul>
-            <form class="d-flex my-2 my-lg-0">
+            <form className="d-flex my-2 my-lg-0">
               <input
-                class="form-control me-sm-2"
+                className="form-control me-sm-2"
                 type="text"
                 placeholder="Cerca Viaggi"
               />
               <button
-                class="btn btn-outline-success my-2 my-sm-0"
+                className="btn btn-outline-success my-2 my-sm-0"
                 type="submit"
               >
                 Cerca
