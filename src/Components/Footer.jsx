08ea1b4a8dc2abf5be796fd7870548faade7a741
Your Name <you@example.com>
2025-08-26 10:56:19 +0200

    fix Footer component and integrate it into the default layout

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return (
+        <footer className="bg-light py-3 border-top text-center">
+            {/* Link */}
+            <div className="mb-2">
+                <a href="#" className="text-decoration-none mx-2 text-dark">Link 1</a>
+                <a href="#" className="text-decoration-none mx-2 text-dark">Link 2</a>
+                <a href="#" className="text-decoration-none mx-2 text-dark">Link 3</a>
+                <a href="#" className="text-decoration-none mx-2 text-dark">Link 4</a>
+            </div>
+            {/* Privacy */}
+            <small className="text-dark">© 2025 - Tutti i diritti riservati</small>
+        </footer>
+    )
+}
--- src/Layouts/DefaultLayout.jsx
+++ src/Layouts/DefaultLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
 import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 export default function DefaultLayout() {
-  return (
-    <>
-      <Header />
-
-      <Outlet />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    );
 }
+
