4400a09ee034ba8e4112c21c5f059185b4697515
Your Name <you@example.com>
2025-08-26 11:32:47 +0200

    style: update Footer component for improved accessibility and visual consistency

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,23 @@
 export default function Footer() {
     return (
-        <footer className="bg-light py-3 border-top text-center">
+        <footer className="bg-light py-3 border-top text-center bg-dark">
             {/* Link */}
             <div className="mb-2">
-                <a href="#" className="text-decoration-none mx-2 text-dark">Link 1</a>
-                <a href="#" className="text-decoration-none mx-2 text-dark">Link 2</a>
-                <a href="#" className="text-decoration-none mx-2 text-dark">Link 3</a>
-                <a href="#" className="text-decoration-none mx-2 text-dark">Link 4</a>
+                <a href="#" className="text-decoration-none mx-2 text-light"><i className="bi bi-instagram"></i></a>
+                <a href="#" className="text-decoration-none mx-2 text-light"><i className="bi bi-twitter"></i></a>
+                <a href="#" className="text-decoration-none mx-2 text-light"><i className="bi bi-facebook"></i></a>
+                <a href="#" className="text-decoration-none mx-2 text-light"><i className="bi bi-google"></i></a>
             </div>
             {/* Privacy */}
-            <small className="text-dark">© 2025 - Tutti i diritti riservati</small>
+
+            <div className="mb-2">
+                <a href="#" className="text-decoration-none mx-2 text-light">Privacy</a>
+                <a href="#" className="text-decoration-none mx-2 text-light">Cookie</a>
+                <a href="#" className="text-decoration-none mx-2 text-light">Termini</a>
+            </div>
+            <hr className="border-light w-75 mx-auto my-3" />
+            <small className="text-light">© 2025 - Tutti i diritti riservati</small>
+
         </footer>
     )
 }
