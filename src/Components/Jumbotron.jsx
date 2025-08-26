60262038d585bc1c8955ef1f18696206934b2e2a
tommaso-mandorino <tommasomandorino@outlook.it>
2025-08-26 16:25:00 +0200

    delete singletrip db, update jumbotron

--- src/Components/Jumbotron.jsx
+++ src/Components/Jumbotron.jsx
@@ -12,26 +12,18 @@ export default function Jumbotron() {
 
         <h1 className="display-6 fw-bold">Ogni strada è un’avventura. Scoprila con Booroad!</h1>
 
-        <div className="py-3">
+        <div className="row py-3 align-items-center">
 
-          <img className="img-fluid float-start me-3" width="650" src={jumbotronImage} alt="Jumbotron image" />
+          <img className="col-6" width="650" src={jumbotronImage} alt="Jumbotron image" />
 
-          <p className="fs-5 mb-5">
-            Booroad è molto più di un semplice sito di viaggi: è il punto di partenza per chi sogna di esplorare il mondo con occhi curiosi e spirito d’avventura.
-            
-            <br />
+          <p className="col-6 fs-5">
 
-            Che tu stia cercando mete fuori dai soliti circuiti turistici, esperienze autentiche a contatto con la cultura locale o itinerari su misura per viaggiatori indipendenti, Booroad ti accompagna passo dopo passo nella creazione del tuo viaggio ideale.
-            
-            <br />
-
-            Preparati a scoprire luoghi sorprendenti, storie da vivere e percorsi che lasciano il segno: ogni viaggio con Booroad inizia con l’ispirazione e si trasforma in un’avventura indimenticabile.
-            
-            <br />
+            Booroad è un sito di viaggi che permette di organizzare facilmente le proprie partenze verso destinazioni popolari,
+             condividendo l’esperienza con altri viaggiatori per vivere il viaggio in compagnia e risparmiare sui costi.
 
             <Link to="tripslist">
 
-              <button className="btn btn-primary btn-lg my-3" type="button">Inizia a viaggiare!</button>
+              <button className="d-block mx-auto btn btn-primary btn-lg my-3" type="button">Inizia a viaggiare!</button>
 
             </Link>
 
