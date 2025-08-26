85643f78f19fc7ecab0015f47c769f5f1ad83820
marzia <you@example.com>
2025-08-26 12:32:01 +0200

    add import

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import DefaultLayout from "./Layouts/DefaultLayout";
 import HomePage from "./Pages/HomePage";
 import Error404 from "./Pages/Error404";
 import TripsList from "./Pages/TripsList";
+import SinglePartecipant from "./Pages/SinglePartecipant";
 
 export default function Main() {
   return (
