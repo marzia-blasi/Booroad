820516077a13be1625330ba134c90c76416bf8ee
marzia <you@example.com>
2025-08-27 12:40:17 +0200

    add road team

--- src/Pages/AboutUsPage.jsx
+++ src/Pages/AboutUsPage.jsx
@@ -4,12 +4,13 @@ export default function AboutUsPage() {
       id: 1,
       title: "Boo...Team",
       text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam quae reprehenderit ipsum nihil, nam natus rerum voluptatum aperiam praesentium dicta rem labore, consequuntur expedita! Libero tempore voluptas debitis nesciunt, provident magni numquam blanditiis iste accusantium unde maiores accusamus eligendi quam nulla deleniti ullam laboriosam veniam doloremque et sequi. Perferendis, illo.",
-      teamone: [
-        <li>Marzia</li>,
-        <li>Asia</li>,
-        <li>Tommaso</li>,
-        <li>Simone</li>,
-      ],
+      crew: [<li>Marzia</li>, <li>Asia</li>, <li>Tommaso</li>, <li>Simone</li>],
+    },
+    {
+      id: 2,
+      title: "Road..team",
+      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam quae reprehenderit ipsum nihil, nam natus rerum voluptatum aperiam praesentium dicta rem labore, consequuntur expedita! Libero tempore voluptas debitis nesciunt, provident magni numquam blanditiis iste accusantium unde maiores accusamus eligendi quam nulla deleniti ullam laboriosam veniam doloremque et sequi. Perferendis, illo.",
+      crew: [<li>Luca</li>, <li>Sara</li>, <li>Davide</li>, <li>Elena</li>],
     },
   ];
 
@@ -17,13 +18,15 @@ export default function AboutUsPage() {
     <>
       <h1>Chi siamo</h1>
       <div className="container d-flex">
-        <img src="https://picsum.photos/id/237/300/300" alt="" />
-        {teams.map(({ id, title, text, teamone }) => (
-          <div className="row ms-4" key={id}>
-            <h3>{title}</h3>
-            <p>{text}</p>
-            <ul>{teamone}</ul>
-          </div>
+        {teams.map(({ id, title, text, crew }) => (
+          <>
+            <img src="https://picsum.photos/id/237/300/300" alt="" />
+            <div className="row ms-4" key={id}>
+              <h3>{title}</h3>
+              <p>{text}</p>
+              <ul>{crew}</ul>
+            </div>
+          </>
         ))}
       </div>
     </>
