43c00292951bd908d1143ff45c500dd4c0dc0936
tommaso-mandorino <tommasomandorino@outlook.it>
2025-08-28 16:17:03 +0200

    fix table responsiveness in partecipants selector

--- src/Components/PartecipantsSelector.jsx
+++ src/Components/PartecipantsSelector.jsx
@@ -4,38 +4,40 @@ export default function PartecipantsSelector() {
 
     return (
 
-        <table className="table">
-            <thead>
-                <tr>
-                    <th className="text-center">Selezionato</th>
-                    <th className="text-center">Nome</th>
-                    <th className="text-center">Cognome</th>
-                    <th className="text-center">Indirizzo email</th>
-                    <th className="text-center">Numero di telefono</th>
-                    <th className="text-center">Codice fiscale</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    partecipants.map(partecipant => {
+        <div className="table-responsive">
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th className="text-center">Selezionato</th>
+                        <th className="text-center">Nome</th>
+                        <th className="text-center">Cognome</th>
+                        <th className="text-center">Indirizzo email</th>
+                        <th className="text-center">Numero di telefono</th>
+                        <th className="text-center">Codice fiscale</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        partecipants.map(partecipant => {
 
-                        return (
+                            return (
 
-                            <tr key={`partecipant-${partecipant.id}`}>
-                                <td className="text-center"><input type="checkbox" className="form-check-input border-dark" id={`partecipant-${partecipant.id}-check`} name={`partecipant-${partecipant.id}-check`} /></td>
-                                <td className="text-center">{partecipant.first_name}</td>
-                                <td className="text-center">{partecipant.last_name}</td>
-                                <td className="text-center">{partecipant.email}</td>
-                                <td className="text-center">{partecipant.phone}</td>
-                                <td className="text-center">{partecipant.tax_code}</td>
-                            </tr>
+                                <tr key={`partecipant-${partecipant.id}`}>
+                                    <td className="text-center"><input type="checkbox" className="form-check-input border-dark" id={`partecipant-${partecipant.id}-check`} name={`partecipant-${partecipant.id}-check`} /></td>
+                                    <td className="text-center">{partecipant.first_name}</td>
+                                    <td className="text-center">{partecipant.last_name}</td>
+                                    <td className="text-center">{partecipant.email}</td>
+                                    <td className="text-center">{partecipant.phone}</td>
+                                    <td className="text-center">{partecipant.tax_code}</td>
+                                </tr>
 
-                        );
+                            );
 
-                    })
-                }
-            </tbody>
-        </table>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
 
     );
 
